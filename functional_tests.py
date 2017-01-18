0b246151a6d9cc74dd3f22f288be8aad787c2d8f
Doojin Kang <doojin.kang@gmail.com>
2017-01-18 20:58:04 +0900

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -12,9 +12,9 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_first_ft(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')

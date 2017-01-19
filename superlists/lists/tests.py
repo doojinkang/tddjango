41ab47dec04da31ba778bcad6e65d4b75c41b799
Doojin Kang <doojin.kang@gmail.com>
2017-01-20 02:44:32 +0900

    Fix package path (for Pycharm)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.template.loader import render_to_string
 
-from lists.views import home_page
+from .views import home_page
 
 class HomePageTest(TestCase):
 

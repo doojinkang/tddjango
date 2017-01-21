ce092b8fec53a8e0aa175c945f9f72f5768dfbf9
Doojin Kang <doojin.kang@gmail.com>
2017-01-22 01:41:08 +0900

    Refactoring view function

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,15 +6,8 @@ from .models import Item
 def home_page(request):
 
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        item = Item()
-        item.text = new_item_text
-        item.save()
+        Item.objects.create(text = request.POST.get('item_text', ''))
         return redirect('/')
-    else:
-        new_item_text = ''
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text,
-    })
+    return render(request, 'home.html')
 

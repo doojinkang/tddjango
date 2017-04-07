bdc503c46c6c33d6112503c295f5b5aa1a9d4384
Doojin Kang <doojin.kang@gmail.com>
2017-04-07 13:28:38 +0900

    AJAX list items

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,12 +3,24 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Item
 
+import json
+from django.core import serializers
+
 def home_page(request):
 
     if request.method == 'POST':
         Item.objects.create(text = request.POST.get('item_text', ''))
         return redirect('/')
 
-    items = Item.objects.all()
+    items = {} # Item.objects.all()
     return render(request, 'home.html', { 'items': items })
+
+def ajax_list(request):
+    items = Item.objects.all()
+    data = serializers.serialize('json', items)
+    return HttpResponse(data)
+    
+#    ret =  '"[{\\"model\\": \\"lists.item\\", \\"pk\\": 1, \\"fields\\": {\\"text\\": \\"Buy peacock feathers\\"}}, {\\"model\\": \\"lists.item\\", \\"pk\\": 2, \\"fields\\": {\\"text\\": \\"Use peacock feathers to make a fly\\"}}]"'
+#    return HttpResponse(ret)
+
 
